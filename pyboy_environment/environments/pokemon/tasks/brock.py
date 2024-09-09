a656a9fd2291fc077e56a4c458339c86089f263e
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2024-09-09 21:24:35 +1200

    added pallet_town, up

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -64,27 +64,38 @@ class PokemonBrock(PokemonEnvironment):
         location_tuple = (current_location["x"], current_location["y"], current_location["map"])
         # print(f"{current_location['x']}, {current_location['y']} - {current_location['map']}")
 
-        # If the current map is "OAKS_LAB"
-        # if current_location['map'] == 'OAKS_LAB,':
-        #     # input("pause")
-        #     # Get the minimum y value from discovered locations in "OAKS_LAB"
-        #     max_y = 0
-        #     for loc in self.discovered_locations:
-        #         if loc[2] == "OAKS_LAB,":
-        #             max_y = max(max_y, loc[1])
-            
-        #     # If current y is less than the minimum y value discovered
-        #     if current_location["y"] > max_y:
-        #         # Add location to discovered locations
-        #         self.discovered_locations.add(location_tuple)
-        #         print("Found new lowest loc")
-        #         return 3.0  # Higher reward for discovering a new lowest location
-
         # Check if the agent is in a new map
         if current_location["map"] not in self.discovered_maps:
             self.discovered_maps.add(current_location["map"])
+            print(f"{current_location['map']}")
             return 10.0  # Reward for discovering a new map
 
+        # If the current map is "OAKS_LAB"
+        if current_location['map'] == 'OAKS_LAB,':
+            # input("pause")
+            # Get the maximum y value from discovered locations in "OAKS_LAB"
+            max_y = 0
+            for loc in self.discovered_locations:
+                if loc[2] == "OAKS_LAB,":
+                    max_y = max(max_y, loc[1])
+            
+            # If current y is less than the minimum y value discovered
+            if current_location["y"] > max_y:
+                # Add location to discovered locations
+                self.discovered_locations.add(location_tuple)
+                print("Found new lowest loc")
+                return 3.0  # Higher reward for discovering a new lowest location
+        
+        elif current_location["map"] == 'PALLET_TOWN,':
+            min_y = 0
+            for loc in self.discovered_locations:
+                if loc[2] == "PALLET_TOWN,":
+                    min_y = min(min_y, loc[1])
+            if current_location["y"] < min_y:
+                self.discovered_locations.add(location_tuple)
+                print("Found new highest loc")
+                return 3.0 # higher reward for discovering a new highest location
+            
         # Check if the specific location (x, y) on the map is new
         if location_tuple not in self.discovered_locations:
             self.discovered_locations.add(location_tuple)
