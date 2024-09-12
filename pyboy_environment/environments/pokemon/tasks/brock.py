ce7cbced83102b1daf5da8767101336ea541282a
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2024-09-12 12:40:46 +1200

    updated reward values

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -19,6 +19,7 @@ class PokemonBrock(PokemonEnvironment):
         self.discovered_locations = set()
         self.discovered_maps = set()
         self.previous_location = None  # Track the previous location
+        self.previous_num_locs = 0  # Track the previous number of discovered locations
 
         valid_actions: list[WindowEvent] = [
             WindowEvent.PRESS_ARROW_DOWN,
@@ -53,38 +54,42 @@ class PokemonBrock(PokemonEnvironment):
     def _get_state(self) -> np.ndarray:
         # Retrieve the current game state
         game_stats = self._generate_game_stats()
-        return [game_stats["badges"]]
+        # print(game_stats["game_area"])
+        num_locs = len(self.discovered_locations)
+        
+        # Return 0 if the number of locations hasn't changed
+        if num_locs == self.previous_num_locs:
+            return [0]
+        else:
+            self.previous_num_locs = num_locs  # Update the previous number of locations
+            return [num_locs]
 
     def _calculate_reward(self, new_state: dict) -> float:
         # Get the current location
         current_location = new_state["location"]
+        location_tuple = (current_location["x"], current_location["y"], current_location["map"])
 
-        # Compare with previous location
-        if self.previous_location is not None and current_location == self.previous_location:
-            reward = -1.0  # Penalize if staying in the same location
-
+        # if location hasn't changed, penalise
+        if current_location == self.previous_location:
+            reward = -10.0
         # if location already visited, penalise
-        # if current_location in self.discovered_locations:
-        #     reward = -1.0
+        elif location_tuple in self.discovered_locations:
+            reward = -1.0  # Penalize for visiting the same location
         else:
-            # Reward logic for discovering new map or new location
-            location_tuple = (current_location["x"], current_location["y"], current_location["map"])
-
-            if current_location["map"] not in self.discovered_maps:
+            # Reward logic for discovering new map or new location that is not oak's lab
+            if current_location["map"] not in self.discovered_maps and current_location["map"] is not "OAKS_LAB,":
                 self.discovered_maps.add(current_location["map"])
                 print(f"Discovered new map: {current_location['map']}")
-                reward = 500.0  # Reward for discovering a new map
-            elif location_tuple not in self.discovered_locations:
+                reward = 10000.0  # Reward for discovering a new map
+            else:
                 self.discovered_locations.add(location_tuple)
                 print(f"Discovered new location: {location_tuple}")
-                reward = 100.0  # Reward for discovering a new location
-            else:
-                reward = 0.0  # No reward if the location is not new
+                reward = 1000.0  # Reward for discovering a new location
 
-            # Update the previous location
-            self.previous_location = current_location
-
+        # Update the previous location
+        self.previous_location = current_location
         return reward
+
 
     def _check_if_done(self, game_stats: dict[str, any]) -> bool:
         # Setting done to true if the agent beats the first gym
